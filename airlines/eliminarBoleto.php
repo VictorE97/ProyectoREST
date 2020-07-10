ea7401900c715d26ca973db1031c550bd272b6e3
VictorE97 <62578107+VictorE97@users.noreply.github.com>
2020-07-10 14:47:00 -0500

    first commit

--- airlines/eliminarBoleto.php
+++ airlines/eliminarBoleto.php
@@ -4,7 +4,7 @@
     ini_set("soap.wsdl_cache_enabled", "0");
     $client = new SoapClient("http://54.197.23.7:8080/aerolinea.wsdl");
     $parametros = array("idBoleto" => $idBoleto);
-    $response = $client->__soapCall('EliminarBoleto', array($parametros));
+    $response = $client->__soapCall('EliminarCompraBoleto', array($parametros));
     
     header("location:Boletos.php");
     //print "<h1>".$response->{'respuesta'}."</h1>";
